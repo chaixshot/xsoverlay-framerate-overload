852de7c6a61b950e37201be8601db14448070c6b
chaixshot <chai5142xshot@gmail.com>
2026-05-15 14:02:33 +0700

    Added: patch PointerScale

---
+++ Patches/PointerScale.cs
@@ -0,0 +1,15 @@
+﻿using HarmonyLib;
+
+namespace xsoverlay_tweak.Patches
+{
+    [HarmonyPatch(typeof(UI_RelativeTransformManipulator))]
+    internal class PointerScale
+    {
+        [HarmonyPatch("OnSetPointerScale")]
+        [HarmonyPostfix]
+        public static void UpdateHoveringOverlay(UI_RelativeTransformManipulator __instance, ref float ___scaleMultiplier)
+        {
+            ___scaleMultiplier = 2f;
+        }
+    }
+}
