b6ee5b12f8fd1ca7ee208611fc6a17f7b866d8be
chaixshot <chai5142xshot@gmail.com>
2026-05-16 10:59:54 +0700

    Fixed: patch RefreshRate scrolling/push/pull tick speed

--- Patches/RefreshRate.cs
+++ Patches/RefreshRate.cs
@@ -6,42 +6,99 @@ namespace xsoverlay_tweak.Patches
 {
     internal class RefreshRate
     {
-        /*[HarmonyPatch(typeof(Unity_Overlay), "DetermineOverlayFramerate")]
+        private static float FrameInterval = 1f / 90f;
+
+        private static float LastGrabTime;
+        private static float GrabbedDistance = 0f;
+
+        private static float LastScrollingTime;
+
+        [HarmonyPatch(typeof(DeviceManager), "Start")]
         [HarmonyPostfix]
-        public static void Update(Unity_Overlay __instance)
+        public static void Start(DeviceManager __instance)
         {
-            if (!__instance.IsDesktopOrWindowCapture) return;
-
-            __instance.ActiveUpdateRateFPS = XConfig.RefreshRate.Value;
-        }*/
-
-        /*[HarmonyPatch(typeof(Unity_Overlay), "UpdateTexture")]
-        [HarmonyPrefix]
-        public static bool UpdateTexture(Unity_Overlay __instance, ref int ___UpdateRateFPS)
-        {
-            if (!__instance.IsDesktopOrWindowCapture) return true;
-
-            ___UpdateRateFPS = -1;
-
-            return true;
-        }*/
+            if (__instance.HMDRefreshRate > 0)
+                FrameInterval = 1f / (float)__instance.HMDRefreshRate;
+        }
 
         [HarmonyPatch(typeof(DeviceManager), "GetHMDRefreshRate")]
         [HarmonyPrefix]
         public static bool GetHMDRefreshRate(DeviceManager __instance)
         {
-            if (XConfig.RefreshRate.Value > __instance.HMDRefreshRate || XConfig.RefreshRate.Value.Equals(-1))
+            XSTools.ExecuteOnMainThread(delegate
             {
-                XSTools.ExecuteOnMainThread(delegate
-                {
-                    Application.targetFrameRate = XConfig.RefreshRate.Value;
-                    Time.fixedDeltaTime = 1f / (float)XConfig.RefreshRate.Value;
-                });
+                Application.targetFrameRate = XConfig.RefreshRate.Value;
+                Time.fixedDeltaTime = 1f / (float)XConfig.RefreshRate.Value;
+            });
 
-                return false;
+            return false;
+        }
+
+        // Fix Push/Pull speed
+        [HarmonyPatch(typeof(Raycaster), "Grab")]
+        [HarmonyPostfix]
+        public static void Grab(ref float ___GrabbedDistance)
+        {
+            float currentTime = Time.unscaledTime;
+            if (currentTime - LastGrabTime < FrameInterval)
+                ___GrabbedDistance = GrabbedDistance;
+            else
+            {
+                GrabbedDistance = ___GrabbedDistance;
+                LastGrabTime = currentTime;
             }
+        }
 
-            return true;
+        // Fix Scrolling speed
+        [HarmonyPatch(typeof(Raycaster), "HandleScrolling")]
+        [HarmonyPrefix]
+        public static bool HandleScrolling(Raycaster __instance, ref MouseInputDevice ___InputDevice, ref int ___ScrollClicksPerSecond, ref float ____tickAccumulator, ref Vector2 ___CursorUVNormalized)
+        {
+            float currentTime = Time.unscaledTime;
+            if (currentTime - LastScrollingTime < FrameInterval)
+            {
+                float num = 0.2f;
+                float ScrollAxis = ___InputDevice.NormalizedScrollAxis.y;
+                float num2 = Mathf.Abs(ScrollAxis);
+
+                if (num2 <= num || (float)___ScrollClicksPerSecond <= 0f)
+                    return false;
+
+                if (__instance.HoveringOverlay.IsDesktopOrWindowCapture)
+                {
+                    ____tickAccumulator += num2 * (float)___ScrollClicksPerSecond * XSettingsManager.Instance.Settings.ScrollSpeed * Time.deltaTime;
+
+                    int num3 = (int)____tickAccumulator;
+                    if (num3 <= 0)
+                        return false;
+
+                    ____tickAccumulator -= (float)num3;
+
+                    if (__instance.HoveringOverlay.IsDesktopOrWindowCapture)
+                    {
+                        MouseOperations.Scroll(((ScrollAxis > 0f) ? 1 : (-1)) * num3, XInputManager.sim);
+                        return false;
+                    }
+                }
+                else if (__instance.HoveringOverlay.IsPluginApplication)
+                {
+                    ____tickAccumulator += num2 * (float)___ScrollClicksPerSecond * XSettingsManager.Instance.Settings.ScrollSpeed * Time.deltaTime;
+
+                    int num3 = (int)____tickAccumulator;
+                    if (num3 <= 0)
+                        return false;
+
+                    ____tickAccumulator -= (float)num3;
+
+                    float num4 = ScrollAxis * (num3 * 0.05f);
+                    __instance.HoveringOverlay.WebViewHandler.WebView.Scroll(new Vector2(0f, -num4), ___CursorUVNormalized);
+                }
+            }
+            else
+                LastScrollingTime = currentTime;
+
+
+            return false;
         }
 
         //!! Test game fps
