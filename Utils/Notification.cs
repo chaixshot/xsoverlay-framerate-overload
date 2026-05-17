7e600ca09bf61bda9079f9975238c5ae24f60a4d
chaixshot <chai5142xshot@gmail.com>
2026-05-17 11:28:52 +0700

    Added: Utils.Notification

---
+++ Utils/Notification.cs
@@ -0,0 +1,36 @@
+﻿using BepInEx;
+using XSOverlay;
+using XSOverlay.Websockets.API;
+
+namespace xsoverlay_tweak.Utils
+{
+    internal class Notification
+    {
+        public static void Send(string title, string content = "", float timeout = 5f)
+        {
+            var notif = new Objects.NotificationObject
+            {
+                title = title,
+                content = content,
+                messageType = 1,
+                timeout = timeout,
+                height = CalculateHeight(content),
+                sourceApp = "XSOverlay Tweak",
+                volume = 0.5f
+            };
+
+            ThreadingHelper.Instance.StartSyncInvoke(() => XSOEventSystem.Current.EventQueueNotification(notif));
+        }
+
+        private static int CalculateHeight(string content)
+        {
+            return content.Length switch
+            {
+                <= 100 => 100,
+                <= 200 => 150,
+                <= 300 => 200,
+                _ => 250
+            };
+        }
+    }
+}
